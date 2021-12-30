231bae5a082de3a84302210b8f1014b6ab6be89b
Jaco5 <jacobartz1987@gmail.com>
2021-12-30 14:27:14 -0800

    day 3 text game complete

--- three-conditional-text-choices-game.py
+++ three-conditional-text-choices-game.py
@@ -33,3 +33,20 @@ Compressing objects: 100% (3/3), █▓▒▒▒▒▒░███████�
                         █▓▓▒▒▒██▓▓▓▒▒▒▒▒▒░░░░░░░░ ▓
                         █▓▓▒▒▒█▓▓▓▒▒▒▒▒▒▒▒░░░░░░░░▓''')
 
+answer = input('''You have finished coding your message and wish to send it to The Hub.
+But first you must package it correctly. WHAT! is the first commandment?''')
+correct = 'git add .'
+if answer!= correct:
+    print('No! You have now been sentenced to 3 hours troubleshooting. Return later to try again')
+elif answer == correct:
+    answer = input('There you go now all your little files are in the box. Now what is the next commandment?')
+    correct = 'git commit -m "..."'
+    if answer != correct:
+        print('No! You have now been sentenced to 3 hours troubleshooting. Return later to try again')
+    elif answer == correct:
+        answer = input('There you go now the entry is commited to the log. Now what is the next commandment?')
+        correct = 'git push origin master'
+        if answer != correct:
+            print('No! You have now been sentenced to 3 hours troubleshooting. Return later to try again')
+        else:
+            print('Yay, you did it!')
