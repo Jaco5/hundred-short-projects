f931fb2f133ce3811ad2f790ad01d5881e71caab
Jaco5 <jacobartz1987@gmail.com>
2022-01-28 13:39:22 -0800

    day 8 cipher, obfuscated, single function with additional mode parameter, error handling

--- eight-cipher.py
+++ eight-cipher.py
@@ -1,40 +1,41 @@
-def encrypt(text, seed):
+import random
+
+def encryption(text, seed, mode):
     encryptedStr = ""
     switch = True
+    seedA = int(seed[0])
+    seedB = int(seed[1])
+    offset = -26
+    if mode == 'd':
+        seedA = -seedA
+        seedB = -seedB
+        offset = -offset
     for ch in text:
         if ch.isalpha():
             if switch:
-                new_ch = ord(ch) + int(seed[0])
+                new_ch = ord(ch) + seedA
                 switch = False
             else:
-                new_ch = ord(ch) + int(seed[1])
+                new_ch = ord(ch) + seedB
                 switch = True
-            if new_ch > ord('z'):
-                new_ch -= 26
+            if new_ch > ord('z') or new_ch < ord('a'):
+                new_ch += offset
             ch = chr(new_ch)
         encryptedStr += ch
     print(encryptedStr)
+    init()
 
-def decrypt(text, seed):
-    decryptedStr = ""
-    switch = True
-    for ch in text:
-        if ch.isalpha():
-            if switch:
-                new_ch = ord(ch) - int(seed[0])
-                switch = False
-            else:
-                new_ch = ord(ch) - int(seed[1])
-                switch = True
-            if new_ch < ord('a'):
-                new_ch += 26
-            ch = chr(new_ch)
-        decryptedStr += ch
-    print(decryptedStr)
- 
-textToCipher = input("Message to be encrypted: ")
-#if left blank generate random
-cipherseed = input("Enter double digit seed: ")
+def init():
+    text = input("Message text: ")
+    if len(text) == 0: init()
+    seed = input("Enter double digit seed: ")
+    if len(seed) != 2: seed = str(random.randint(10,99)); print("Invalid seed. " + str(seed) + " used instead.")
+    mode = input("""Options:
+'e' - encrypt mode
+'d' - decrypt mode
 
-encrypt(textToCipher, cipherseed)
+>""")
+    if mode != 'e' and mode != 'd': print("Must enter a mode. Resetting."); init()
+    else: encryption(text, seed, mode)
 
+init()
