d34fa863a256818addfc6d866e71e3101d1fb6c0
Jaco5 <jacobartz1987@gmail.com>
2022-01-29 18:40:15 -0800

    circle animation

--- seventeen-class-constructor.py
+++ seventeen-class-constructor.py
@@ -14,7 +14,7 @@ class Entity:
     def __init__(self):
         self.name = name_gen()
         self.number = num(100)
-        self.vector = [0,0]
+        self.vector = [10,15]
         self.location = [num(1000), num(1000)]
         print(f"{self.number} {self.name} {self.location}")
 
@@ -27,11 +27,17 @@ def create_group(size):
     #print(f"group {group}")
 create_group(10)
 #Tk(screenName=None, baseName=None, className='Tk', usTk=1)
+
 window = tkinter.Tk()
 window.title("Window Title")
 canvas = tkinter.Canvas(window, width=1000, height=1000)
-for each in group:
-    oval = canvas.create_oval(each.location[0], each.location[1], each.location[0]+each.number, each.location[1]+each.number, fill='green')
 
-canvas.pack()
-window.mainloop()
+for each in group:
+    canvas.create_oval(each.location[0], each.location[1], each.location[0]+each.number, each.location[1]+each.number, fill='green')
+
+for each in group:
+    each.location[0] = each.location[0] + each.vector[0]
+    each.location[1] = each.location[1] + each.vector[1] 
+
+    canvas.pack()
+    window.mainloop()
